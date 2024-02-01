b40afa1c5e02cdcc8e676135fa37b389060ad004
Jhon Achata <jhonachata.dev@gmail.com>
2024-01-31 22:56:12 -0500

    update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
 app.listen(process.env.APP_PORT, () => {
     console.log(`Server is running on port ${process.env.APP_PORT}`);
 });
